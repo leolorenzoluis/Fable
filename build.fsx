dbe2ec3d6834c2d54d373ace6d6b33b18bc22416
lllpsi <leolorenzoluis@outlook.com>
2017-05-13 14:12:57 -0400

    Bump version to 1.0.0-narumi-915

--- build.fsx
+++ build.fsx
@@ -512,8 +512,8 @@ Target "GitHubRelease" (fun _ ->
     Git.Commit.Commit "" (sprintf "Bump version to %s" release.NugetVersion)
     Branches.pushBranch "" remote (Information.getBranchName "")
 
-    Branches.tag "" release.NugetVersion
-    Branches.pushTag "" remote release.NugetVersion
+    Branches.tag "" "1.0.0-blah"
+    Branches.pushTag "" remote "1.0.0-blah"
 
     // release on github
     createClient user pw
